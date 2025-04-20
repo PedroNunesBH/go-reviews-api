152a725d954745264f3b1f1946f0916c3e8bde41
Pedro Nunes <pedrohenriquestore99@gmail.com>
2025-04-20 10:04:03 -0300

    feat: add ReviewDB struct for data persistence

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,15 @@
-package main 
+package main
+
+import (
+	"github.com/PedroNunesBH/go-reviews-api/internal/entity"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	
+	db, err := gorm.Open(sqlite.Open("reviews.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Restaurant{}, &entity.Review{})
 }
---
+++ internal/infra/database/review_db.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"gorm.io/gorm"
+	"github.com/PedroNunesBH/go-reviews-api/internal/entity"
+	pkgEntity "github.com/PedroNunesBH/go-reviews-api/pkg/entity"
+
+)
+
+type ReviewDB struct {
+	DB *gorm.DB
+}
+
+func NewReviewDB(db *gorm.DB) *ReviewDB {
+	return &ReviewDB{DB: db}
+}
+
+func (r *ReviewDB) FindAll() ([]*entity.Review, error) {
+	var reviews []*entity.Review
+	result := r.DB.Find(&reviews)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return reviews, nil
+}
+
+func (r *ReviewDB) CreateReview(review *entity.Review) error {
+	return r.DB.Create(&review).Error
+}
+
+func (r *ReviewDB) FindReviewByID(id pkgEntity.ID) (*entity.Review, error) {
+	var review *entity.Review
+	if err := r.DB.Where("ID = ?", id).First(&review).Error; err != nil {
+		return nil, err
+	}
+	return review, nil
+}
+
+func (r *ReviewDB) DeleteReview(id pkgEntity.ID) error {
+	result := r.DB.Where("id = ?", id).Delete(&entity.Review{})
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *ReviewDB) UpdateReview(review *entity.Review) error {
+	result := r.DB.Model(&entity.Review{}).
+        Where("id = ?", review.ID).
+        Updates(map[string]interface{}{
+            "description":     review.Description,
+            "rating":    review.Rating,
+            "restaurant_id": review.RestaurantID,
+        })
+
+    return result.Error
+}
