dc842396c35a82c30a54357fe36e2624b7355bf1
Pedro Nunes <pedrohenriquestore99@gmail.com>
2025-04-27 18:03:06 -0300

    feat: add User entity

---
+++ internal/entity/user.go
@@ -0,0 +1,43 @@
+package entity 
+
+import (
+	"github.com/PedroNunesBH/go-reviews-api/pkg/entity"
+	"errors"
+)
+
+type User struct {
+	ID  entity.ID
+	Username  string
+	Email  string 
+	Password string
+}
+
+var ErrInvalidUsername error = errors.New("username is required")
+var ErrInvalidEmail error = errors.New("email is required")
+var ErrInvalidPassword error = errors.New("password must have at least 8 characters")
+
+func NewUser(username, email, password string) (*User, error) {
+	user := User{ID: entity.NewID(),
+		Username: username,
+		Email: email,
+		Password: password,
+	}
+	err := user.ValidateUser()
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (u *User) ValidateUser() error {
+	if u.Username == "" {
+		return ErrInvalidUsername
+	}
+	if u.Email == "" {
+		return ErrInvalidEmail
+	}
+	if u.Password == "" || len(u.Password) < 8 {
+		return ErrInvalidPassword
+	}
+	return nil
+}
