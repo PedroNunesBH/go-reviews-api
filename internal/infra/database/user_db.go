b6a105193e427a8844a85113400c3c50a9cd81f3
Pedro Nunes <pedrohenriquestore99@gmail.com>
2025-04-28 11:50:06 -0300

    feat: add method to create user in database

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"gorm.io/gorm"
+	"github.com/PedroNunesBH/go-reviews-api/internal/entity"
 )
 
 type UserDB struct {
@@ -12,4 +13,8 @@ func NewUserDB(db *gorm.DB) *UserDB {
 	return &UserDB{
 		DB: db,
 	}
+}
+
+func (u *UserDB) CreateUser(user *entity.User) error {
+	return u.DB.Create(&user).Error
 }
---
+++ internal/infra/database/user_db_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/PedroNunesBH/go-reviews-api/internal/entity"
+	"gorm.io/gorm"
+	"github.com/glebarez/sqlite"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error()
+	}
+	db.AutoMigrate(&entity.User{})
+	userDB := NewUserDB(db)
+
+	user, err := entity.NewUser("Userteste", "teste@gmail.com", "teste234")
+	assert.Nil(t, err)
+	err = userDB.CreateUser(user)
+	assert.Nil(t, err)
+	var userFound entity.User
+	err = db.First(&userFound, "id = ?", user.ID).Error
+	assert.Nil(t, err)
+
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Username, userFound.Username)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.Equal(t, user.Password, userFound.Password)
+}
